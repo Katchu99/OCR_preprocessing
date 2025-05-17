53904fd59279d0b0a55573cf4fa090f363a6f8bd
Katchu99 <Maariius@outlook.de>
2025-05-17 23:12:21 +0200

    minor changes

--- main.py
+++ main.py
@@ -13,16 +13,11 @@ import image_utils
 project_root = Path(__file__).parent
 input_path: Path = project_root.joinpath(project_root, "Input") # Input Folder path
 output_path: Path = project_root.joinpath(project_root, "Output") # Output Folder path
-file_path: list[Path] = file_utils.set_args(input_path) # Path to file to be processed
+file_paths: list[Path] = file_utils.set_args(input_path) # Path to file to be processed
 
 
 if __name__ == "__main__":
-    file_utils.file_exists(file_path) # throws FileNotFoundError if file does not exist
-    file_type = file_utils.get_file_type(file_path) # throws TypeError if file type is not supported
+    file_utils.file_exists(file_paths) # throws FileNotFoundError if file does not exist
+    file_utils.check_file_type(file_paths) # throws TypeError if file type is not supported and converts pdfs to images
+    image_paths = file_utils.get_image_paths()
 
-    if file_type == "pdf":
-        img_paths = image_utils.pdf_to_image(file_path)
-        for img_path in img_paths:
-            img = Image.open(img_path)
-            img.save(output_path.joinpath(img_path.stem + ".jpg"), "JPEG")
-    
